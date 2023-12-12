5d7a35258c4774f2f42cfcd2b5e8df22bb57e952
tpougy <thomazpougy@gmail.com>
2023-12-11 23:08:29 -0300

    testando outra possibilidade

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="streamlit_azure_login",
-    version="1.0.11",
+    version="1.0.12",
     author="Thomaz Pougy",
     author_email="tp@rbrasset.com.br",
     description="Componente streamlit para autenticar usuários no azure AD",
@@ -10,7 +10,7 @@ setuptools.setup(
     long_description_content_type="text/plain",
     url="",
     packages=setuptools.find_packages(),
-    package_data={"streamlit_azure_login": ["frontend/**/*", "frontend/**"]},
+    package_data={"streamlit_azure_login": ["frontend/**/*", "frontend/**/**/*"]},
     include_package_data=True,
     classifiers=[],
     python_requires=">=3.6",
